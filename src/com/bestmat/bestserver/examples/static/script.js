5a849ef2b71034eeaffbb362bc5ad4016907adb0
BestMat <76582849+BestMat@users.noreply.github.com>
2024-05-02 12:08:54 +0530

    Update script.js

--- src/com/bestmat/bestserver/examples/static/script.js
+++ src/com/bestmat/bestserver/examples/static/script.js
@@ -1,4 +1,20 @@
 // © 2024 - BestDeveloper - BestMat, Inc. - All rights reserved.
+document.write(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BestServer StaticFileRoutes Java Example</title>
+    <script src="./script.js" defer></script>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <button class="button">Click Me</button>
+</body>
+</html>
+`);
+
 const button = document.querySelector(".button");
 
 button.addEventListener("click", function(){
